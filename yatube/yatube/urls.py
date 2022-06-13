9c98932473cb83c846686ecd448e18acd7e1f0b6
ks-grshkv <kseniia.grshkv@yandex.ru>
2022-06-14 00:19:34 +0300

    Fixed minor bugs 4

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,6 +15,11 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
 ]
 
+
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
